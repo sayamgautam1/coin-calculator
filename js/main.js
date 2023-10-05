8cfe4b788af087d334c8f665ec7ee6a826253af2
Sayam Gautam <sayamgautam1@gmail.com>
2023-10-05 14:43:48 +1030

    write a function to generate change for a given user input and display the change to the respective coint field

--- js/main.js
+++ js/main.js
@@ -9,18 +9,46 @@ function calculateChange() {
   // split the input value from the decimal point
   let [dollars, cents] = inputAmount.split(".");
 
-  // convert the recieved value into floats
+  // convert the recieved value into floats , check if the value is null or 0
 
-  let dollarsFloat = parseFloat(dollars);
-  let centsFloat = cents ? parseFloat(cents) : 0; // Default to 0 if cents is not entered
+  let dollarsFloat = dollars ? parseFloat(dollars) : 0;
+  let centsFloat = cents ? parseFloat(cents) : 0;
 
   //   console.log(dollarsFloat);
   //   console.log(centsFloat);
 
   // Calculate the total amount in cents
   let totalCents = dollarsFloat * 100 + centsFloat;
+
+  // Initialize an object to store the count of each coin
+  const totalCoins = {};
+
+  // Iterate through the available coins
+  for (const coinValue of coins) {
+    // Calculate the number of coins of this value that can be used
+    const count = Math.floor(totalCents / coinValue);
+
+    // Store the count, even if it's zero
+    if (coinValue >= 100) {
+      totalCoins[coinValue / 100 + "s"] = count;
+    } else {
+      totalCoins[coinValue + "c"] = count;
+    }
+
+    // Update the total amount
+    totalCents -= count * coinValue;
+  }
+
+  console.log(totalCoins, "result");
+  // Display counts in their respective div elements
+  for (var key in totalCoins) {
+    var div = document.getElementById(key + "-output");
+    if (div) {
+      div.innerHTML = key + ": " + totalCoins[key];
+    }
+  }
 }
 
-// add event listener to listent to claculate change button inside the dom
+// add event listener to listent to calculate change button inside the dom
 var buttonElement = document.getElementById("calculate-change");
 buttonElement.addEventListener("click", calculateChange);
--- test/main.test.js
+++ test/main.test.js
@@ -11,7 +11,7 @@ global.document = dom.window.document;
 global.window = dom.window;
 
 // Import the code to be tested
-const { calculateChange } = require("../js/main.js"); // Replace with the correct path
+const { calculateChange } = require("../js/main.js");
 
 describe("Coin Change Calculator User Interaction", () => {
   let inputAmount;
@@ -21,6 +21,7 @@ describe("Coin Change Calculator User Interaction", () => {
   beforeEach(() => {
     inputAmount = document.getElementById("total-amount");
     calculateChange = document.getElementById("calculate-change");
+    coinDivs = Array.from(document.getElementsByClassName("coin"));
   });
 
   it("should have value for user input", () => {
@@ -42,5 +43,29 @@ describe("Coin Change Calculator User Interaction", () => {
 
     // Expect that the calculateChange function was called once
     expect(mockCalculateChange).toHaveBeenCalledTimes(1);
+  });
+
+  it("should calculate change correctly for $1", () => {
+    // Set input values to $3.75
+    inputAmount = "1";
+
+    // Trigger a click event on the button
+    const event = new dom.window.Event("click");
+    calculateChange.dispatchEvent(event);
+
+    // Define the expected change as an array of strings for each coin denomination
+    const expectedChange = [
+      "2s: 0",
+      "1s: 1",
+      "50c: 0",
+      "20c: 0",
+      "10c: 0",
+      "5c: 0",
+      "2c: 0",
+      "1c: 0",
+    ];
+
+    // Check if the displayed change matches the expected change
+    expect(coinDivs.map((div) => div.innerHTML)).toEqual(expectedChange);
   });
 });
